9754974f5ace3d83c2b7adf1ee5f9e734f6e1eda
Andrey Belyaev <andrey.belyaev@jetbrains.com>
2025-01-20 19:20:41 +0100

    Removed unused imports

--- src/main/java/org/jetbrains/test/demomulticontext/coffee/components/ComponentsConfiguration.java
+++ src/main/java/org/jetbrains/test/demomulticontext/coffee/components/ComponentsConfiguration.java
@@ -1,12 +1,10 @@
 package org.jetbrains.test.demomulticontext.coffee.components;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @ComponentScan
 @Configuration
